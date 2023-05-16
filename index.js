fb731c43c28b32fa35143656a863728f4162d24e
Monica Ramesh <monica98r@gmail.com>
2023-05-16 14:43:56 -0400

    created myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+  let num1 = 31;
+  let num2 = 2;
+
+  let multiply = num1 * num2;
+
+  let random = Math.floor(Math.random()+ 1);
+
+let num3= 19;
+let num4= 5;
+
+let mod = (num3 % num4);
+
+let max=20;
+Math.max(5,10,15,20);
+
